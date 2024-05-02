fd8726b541f863bb7cdaebceb16c968374c96bf1
=ebru <=ebru.ayyilmaz@gmail.com>
2024-05-02 21:34:19 +0300

    first-commit

--- netflix/settings.py
+++ netflix/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-1l8r!c@t4kve^p^d(xh6dqxyw#0-vr!wnke^^royx^)rx@b2f#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hakan.pythonanywhere.com']
 
 
 # Application definition
@@ -129,6 +129,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+STATIC_ROOT = os.path.join(BASE_DIR/'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
