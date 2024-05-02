38380f02baa26040fbd93fe56dee0e41c6a5cbd2
=ebru <=ebru.ayyilmaz@gmail.com>
2024-05-02 20:29:00 +0300

    first commit

---
+++ movies/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from user.forms import *
+
+class Profiles(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+    isim = models.CharField(max_length=100)
+    
+    resim = models.FileField(upload_to='profiles/',verbose_name="Profil Resmi", null=True)
+    
+    def __str__(self):
+        return self.isim
+    
+class Kategori(models.Model):
+    isim = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.isim
+
+class Movie(models.Model):
+    isim = models.CharField(max_length=50)
+    resim = models.FileField(upload_to="moviegif/", verbose_name="Film Resmi", null=True)
+    video = models.FileField(upload_to="videolar/", verbose_name="Film Videosu", null=True)
+    kategori = models.ForeignKey(Kategori,on_delete=models.CASCADE,null=True)
+
+    def __str__(self):
+        return self.isim
+    
---
+++ user/views.py
@@ -0,0 +1,140 @@
+from django.shortcuts import render,redirect
+from .forms import *
+from django.contrib.auth import authenticate,login,logout,update_session_auth_hash
+from django.contrib import messages
+from .models import *
+from django.contrib.auth.forms import PasswordChangeForm
+from django.core.mail import send_mail
+from django.conf import settings
+
+# Create your views here.
+
+def userRegister(request):
+
+    # form = UserForm()
+    # if request.method == "POST":
+    #     form = UserForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         messages.success(reques,"Başarıyla kayıt olundu.")
+    #         return redirect('login')
+        
+    # context = {
+    #     'form':form
+    # }
+
+    if request.method == "POST":
+        isim = request.POST['isim']
+        soyisim = request.POST['soyisim']
+        email = request.POST['email']
+        resim = request.FILES['resim']
+        tel = request.POST['tel']
+        dogum = request.POST['dogum']
+        sifre1 = request.POST['sifre1']
+        sifre2 = request.POST['sifre2']
+
+        if sifre1 == sifre2:
+            if User.objects.filter(email = email).exists():
+                messages.error(request,"Bu mail kullanılıyor.")
+                return redirect('register')
+            elif len(sifre1)<= 8:
+                messages.error(request,"Şifre 8 karakterden kısa olamaz.")
+                return redirect('register')
+            elif '!' in isim or '?' in isim:
+                messages.error(request,"İsimde özel karakter kullanılamaz.")
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=email, email=email, password=sifre1)
+
+                Kullanici.objects.create(
+                    user = user,
+                    isim = isim,
+                    soyisim = soyisim,
+                    email = email,
+                    resim = resim,
+                    tel = tel,
+                    dogum = dogum,
+
+                )
+
+                user.save()
+                
+                subject = "deneme yapıyorum"
+                message = "deneme yaptım"
+                send_mail(
+                    subject,
+                    message,
+                    settings.EMAIL_HOST_USER,
+                    [user.email]
+                )
+
+
+                messages.success(request,"Kullanıcı başarıyla oluşturuldu.")
+                return redirect('login')
+
+    return render(request, "register.html")
+
+
+def userLogin(request):
+
+
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username = username, password = password)
+
+        if user is not None:
+            login(request,user)
+            messages.success(request, "Başarıyla giriş yaptınız.")
+            return redirect('profiles')
+        else:
+            messages.error(request, "Kullanıcı adı ve ya şifre hatalı.")
+            return redirect('login')
+
+    return render(request, 'login.html')
+
+
+def userLogout(request):
+    logout(request)
+    messages.success(request,"Başarıyla çıkış yapıldı.")
+    return redirect('login')
+
+def hesap(request):
+
+    user = request.user.kullanici
+
+    context = {
+        'user':user
+    }
+
+    return render(request,'hesap.html',context)
+
+def passwordChange(request):
+
+    if request.method == "POST":
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request,request.user)
+            logout(request)
+            messages.success(request,"Şifre başarıyla değiştirildi.")
+            return redirect('login')
+        else:
+            messages.error(request,"Başarısızsın.")
+    else:
+        form = PasswordChangeForm(request.user)
+        
+    context = {
+        'form':form
+    }
+
+    return render(request,"password_change.html",context)
+
+def accountDelete(request,):
+    if request.method == "POST":
+        user = request.user
+        user.delete()
+        messages.success(request,"Hesabınız başarı ile silindi.")
+        return redirect("register")
+    return render(request,"account_delete.html")
